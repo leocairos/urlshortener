50049ef4ac6e219242b5764fcfe6f15ff76e400d
Leonardo Sampaio Cairo <leocairos@gmail.com>
2020-11-27 19:55:29 -0300

    Frontend, the first commit

---
+++ frontend/README.md
@@ -0,0 +1,10 @@
+# URL SHORTENER
+
+URL Shortener Project (Frontend)
+React JS + Bootstrap
+
+## How To
+
+### Create a project React JS
+
+$ npx create-react-app frontend
---
+++ frontend/src/pages/HomePage/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <p>URL Shortener</p>
+    )
+  }
+}
+
+export default HomePage;
